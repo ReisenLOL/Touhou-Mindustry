af756d71a21660feb33b1b5c067e5c6658607a03
ReisenLOL <marisathestealer@gmail.com>
2025-03-02 17:39:10 -0800

    Summary (Required)

--- Assets/Scripts/Router.cs
+++ Assets/Scripts/Router.cs
@@ -14,35 +14,27 @@ public class Router : MonoBehaviour
     void Update()
     {
         int randomIndex = Random.Range(0, conveyorChecks.Length);
-        Collider2D conveyor = null;
-        if (DetectConveyors(randomIndex) is not null and Collider2D c)
+        Conveyor c = null;
+        Collider2D col = DetectConveyors(randomIndex);
+        Debug.Log(col);
+        if (col != null && col != this && col.transform != null && col.transform.TryGetComponent(out c))
         {
-            Debug.Log("1");
-            conveyor = c;
-        }
-        if (conveyor == null)
-        {
-            Debug.Log("2");
-            return;
-        }
-        nextConveyorCheck = conveyor.transform;
-        foreach (var item in moveResource)
-        {
-            Debug.Log("3");
-            if (item == null)
+            nextConveyorCheck = c.transform;
+            foreach (var item in moveResource)
             {
-                Debug.Log("4");
-                continue;
+                if (item == null)
+                {
+                    continue;
+                }
+                item.MoveToNextConveyor(nextConveyorCheck, movementSpeed);
             }
-            item.MoveToNextConveyor(nextConveyorCheck, movementSpeed);
+            Debug.Log(moveResource.Count);
         }
-        Debug.Log(moveResource.Count);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform != null && collision.transform.TryGetComponent(out MoveResource output))
         {
-            Debug.Log("5");
             moveResource.Add(output);
         }
     }
@@ -50,13 +42,11 @@ public class Router : MonoBehaviour
     {
         if (collision.transform != null && collision.transform.TryGetComponent(out MoveResource output))
         {
-            Debug.Log("6");
             moveResource.Remove(output);
         }
     }
     private Collider2D DetectConveyors(int index)
     {
-        Debug.Log("7");
-        return Physics2D.OverlapCircle(this.conveyorChecks[index].position, 0.05f, conveyorLayer);
+        return Physics2D.OverlapCircle(this.conveyorChecks[index].position, 5f, conveyorLayer);
     }
 }
