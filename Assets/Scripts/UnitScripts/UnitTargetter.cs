dfabf4c847f55ef9b85b2bc4aea8f5dd516dd26c
ReisenLOL <marisathestealer@gmail.com>
2025-03-18 17:35:07 -0700

    fix alloyer

--- Assets/Scripts/UnitScripts/UnitTargetter.cs
+++ Assets/Scripts/UnitScripts/UnitTargetter.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class UnitTargetter : MonoBehaviour
 {
@@ -12,9 +10,6 @@ public class UnitTargetter : MonoBehaviour
     private GameObject closestTarget;
     private UnitController unitController;
     private UnitStats unitStats;
-    private AStarPathfinder pathfinder;
-    private List<Node> path;
-    private int pathIndex = 0;
     public float range;
     [SerializeField] LayerMask unitLayer;
     [SerializeField] LayerMask playerLayer;
@@ -22,51 +17,29 @@ public class UnitTargetter : MonoBehaviour
     void Start()
     {
         core = GameObject.Find("Core");
-        pathfinder = GameObject.Find("GameManager").GetComponent<AStarPathfinder>();
         player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody2D>();
         unitController = GetComponent<UnitController>();
         unitStats = GetComponent<UnitStats>();
         isEnemy = unitStats.isEnemy;
         closestTarget = this.gameObject;
-        path = pathfinder.FindPath(transform.position, core.transform.position);
-        if (path == null)
-        {
-            Debug.LogError("Enemy path is NULL at Start!");
-        }
-        else
-        {
-            Debug.Log("Enemy path found with " + path.Count + " nodes.");
-        }
     }
 
     void Update()
     {
         if (isEnemy)
         {
-            path = pathfinder.FindPath(transform.position, core.transform.position);
-            if (path == null || pathIndex >= path.Count)
+            float distanceToPlayer = Vector3.SqrMagnitude(player.transform.position - transform.position);
+            float distanceToCore = Vector3.SqrMagnitude(core.transform.position - transform.position);
+            if (distanceToPlayer < distanceToCore)
             {
-                return;
+                lookDirection = (player.transform.position - transform.position).normalized;
+                unitController.lookDirection = lookDirection;
             }
-            if (path == null || path.Count == 0)
+            else
             {
-                Debug.LogWarning("Path is NULL or empty! Enemy has nowhere to go.");
-                return;
-            }
-
-            if (pathIndex >= path.Count)
-            {
-                Debug.Log("Enemy reached end of path.");
-                return;
-            }
-            Vector3 targetPos = path[pathIndex].worldPosition;
-            lookDirection = targetPos;
-
-            if (Vector3.Distance(transform.position, targetPos) < 0.1f)
-            {
-                pathIndex++;
-                Debug.Log("Enemy moving to next waypoint: " + pathIndex);
+                lookDirection = (core.transform.position - transform.position).normalized;
+                unitController.lookDirection = lookDirection;
             }
         }
         else
@@ -74,7 +47,7 @@ public class UnitTargetter : MonoBehaviour
             lookDirection = (closestTarget.transform.position - transform.position).normalized;
             unitController.lookDirection = lookDirection;
         }
-        unitController.lookDirection = lookDirection;
+
         targetList = DetectTargets();
         if (targetList.Length == 0 || closestTarget == null)
         {
